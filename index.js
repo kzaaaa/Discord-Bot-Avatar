eccdd9e545ffcabaf3dd761bb5617a739c52dbdc
kza <50947369+kzaaaa@users.noreply.github.com>
2021-09-13 16:17:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-  const Discord = require('discord.js')
+const Discord = require('discord.js')
 const client  = new Discord.Client()
 const config = require('./config.json')
+
 client.on('ready', async () => {
-setInterval(() => {
+  setInterval(() => {
   let guild = client.guilds.cache.get(config.guild)
-  if(!guild) return console.log('Guild introuvable')
-    let channel = guild.channels.cache.get(config.salon)
-    if(!channel) return console.log('Salon introuvable')
-    if(channel) {  
+  
+     if(!guild) return console.log('Guild introuvable')
+    
+  let channel = guild.channels.cache.get(config.salon)
+     if(!channel) return console.log('Salon introuvable')
+     if(channel) {  
+       
 const user = client.users.cache.random()
+
 if(user.bot) return;
+       
 const embed = new Discord.MessageEmbed()
 .setImage(user.displayAvatarURL({ dynamic : true  }))
-.setColor(`#2f3136`)
+.setColor(RED)
 
 channel.send(embed);
 }
+}, 10000)})
 
-
-}, 20000)})
-client.login(config.token)
+client.login(config.TOKEN)
